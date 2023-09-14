ab6dc6503aa705be11e811c4d5898172179314b3
Rajkanwar Singh <rajkanwarsingh15@gmail.com>
2023-09-15 01:10:49 +0530

    Time: 1 ms (94.57%), Space: 44.4 MB (53.32%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int len = nums.length;
+        int[] num = new int[2*len];
+        for(int i=0;i<len;i++){
+            num[i]=nums[i];
+            num[i+len]=nums[i];
+        }
+        return num; 
+    }
+}
